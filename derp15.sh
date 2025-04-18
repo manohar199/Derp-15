b5a23740d9c7054f342ca874529821fecff6b39f
manohar199 <77186340+manohar199@users.noreply.github.com>
2025-04-18 22:52:08 +0530

    Update derp15.sh

--- derp15.sh
+++ derp15.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -rf .repo/local_manifests
 repo init -u https://github.com/The-Clover-Project/manifest.git -b 15-qpr2 --git-lfs
 git clone https://github.com/manohar199/Project-Matrixx_manfests  --depth 1 -b main .repo/local_manifests 
