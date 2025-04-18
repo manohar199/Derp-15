0f5b714b2d06661749c96529a098b481dd3e143f
manohar199 <77186340+manohar199@users.noreply.github.com>
2025-04-18 23:49:03 +0530

    Update derp15.sh

--- derp15.sh
+++ derp15.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-rm -rf .repo/local_manifests
-repo init -u https://github.com/The-Clover-Project/manifest.git -b 15-qpr2 --git-lfs
-git clone https://github.com/manohar199/Project-Matrixx_manfests  --depth 1 -b main .repo/local_manifests 
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 15-qpr2 --git-lfs 
 /opt/crave/resync.sh 
 source build/envsetup.sh 
 lunch aosp_bramble-bp1a-userdebug
