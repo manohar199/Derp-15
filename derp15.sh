d8304a7d78d030c1165c4a23817633c5c23991ea
manohar199 <77186340+manohar199@users.noreply.github.com>
2025-04-18 22:49:08 +0530

    Update clove.sh

--- derp15.sh
+++ derp15.sh
@@ -1,12 +1,7 @@
 rm -rf .repo/local_manifests
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 15-qpr2 --git-lfs
 git clone https://github.com/manohar199/Project-Matrixx_manfests  --depth 1 -b main .repo/local_manifests 
-echo "repo sync" 
 /opt/crave/resync.sh 
-echo "envsetup.sh"
 source build/envsetup.sh 
-echo "breakfast or lunch" 
-lunch aosp_bramble-userdebug
-echo "Build the code" 
-#make installclean
+lunch aosp_bramble-bp1a-userdebug
 mka bacon
