fab95af16b28565bc4b93286d7f9a42118ccffa4
manohar199 <77186340+manohar199@users.noreply.github.com>
2025-04-19 06:09:07 +0530

    Update derp15.sh

--- derp15.sh
+++ derp15.sh
@@ -2,5 +2,5 @@
 repo init -u https://github.com/The-Clover-Project/manifest.git -b 15-qpr2 --git-lfs 
 /opt/crave/resync.sh 
 source build/envsetup.sh 
-lunch aosp_bramble-bp1a-userdebug
+lunch aosp_bramble-ap4a-user
 mka bacon
