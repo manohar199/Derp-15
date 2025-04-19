d95b2d540c4af581a1c4d7083a2a442b91c4fed9
manohar199 <77186340+manohar199@users.noreply.github.com>
2025-04-19 11:43:08 +0530

    Update derp15.sh

--- derp15.sh
+++ derp15.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 repo init -u https://github.com/The-Clover-Project/manifest.git -b 15-qpr2 --git-lfs 
 /opt/crave/resync.sh 
-source build/envsetup.sh 
-lunch clover_bramble-ap4a-userdebug
+. ./build/envsetup.sh
+export TARGET_RELEASE=ap4a
+build_build_var_cache
+lunch
 mka bacon
